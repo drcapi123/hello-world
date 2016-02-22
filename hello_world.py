99a5234632ad3d1a3311092583769e17b2b64ab6
drcapi123 <drcapi123@gmail.com>
2016-02-22 13:48:15 -0500

    commit

--- hello_world.py
+++ hello_world.py
@@ -6,7 +6,7 @@ class Solver:
         while True:
             a = int(input("a "))
             b = int(input("b "))
-            c = int(input("e "))
+            x = int(input("e "))
             d = b ** 2 - 4 * a * c
             if d >= 0:
                 dis = math.sqrt(d)
