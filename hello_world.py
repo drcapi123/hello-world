28c026c9a838290fbb09cf09c2fc705c3aa27172
drcapi123 <drcapi123@gmail.com>
2016-02-22 13:41:22 -0500

    commit

--- hello_world.py
+++ hello_world.py
@@ -9,7 +9,7 @@ class Solver:
             c = int(input("c "))
             d = b ** 2 - 4 * a * c
             if d >= 0:
-                disc = math.sqrt(d)
+                dis = math.sqrt(d)
                 root1 = (-b + disc) / (2 * a)
                 root2 = (-b - disc) / (2 * a)
                 print(root1, root2)
